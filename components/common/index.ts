d62417e38639d9d006ace22ff403569c3a405360
TELLOWSS <pshkmj@gmail.com>
2026-05-31 12:22:50 +0900

    feat(A3-1): add premium safety common UI components

---
+++ components/common/index.ts
@@ -0,0 +1,8 @@
+export { StatusPill } from './StatusPill';
+export { MetricCard } from './MetricCard';
+export { RiskBadge } from './RiskBadge';
+export { WorkTypeBadge } from './WorkTypeBadge';
+export { EmptyState } from './EmptyState';
+export { LoadingSkeleton } from './LoadingSkeleton';
+export { SectionCard } from './SectionCard';
+export { InsightCard } from './InsightCard';
